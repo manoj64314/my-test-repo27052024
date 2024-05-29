ed3e4e78598c342e185582f83e03ca6b13d223c5
MANOJ PANDEY <104612547+manoj64314@users.noreply.github.com>
2024-05-29 12:43:58 +0530

    Update Hello-world.java deleted three lines

--- Hello-world.java
+++ Hello-world.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
 	System.out.println("Hello, World");
-	System.out.println("Hello, Manoj");
-	System.out.println("Hello, Vishant");
-	  System.out.println("Hello, Vishal");
-	  }
+		  }
 }
